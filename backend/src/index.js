ca0be161abe7313309f98130b03991d5c8af2cf2
Priyanka Chourey <priyanka.choure10@gmail.com>
2023-08-22 11:52:34 +0530

    connect server to mongodb database

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,7 +1,16 @@
+const mongoose = require("mongoose");
 const app = require("./app");
-const PORT = 8082;
-let server;
+const config = require("./config/config");
 
-server = app.listen(PORT, () => {
-  console.log("listing to port", PORT);
-});
+let server;
+console.log(config.mongoose);
+// TODO: CRIO_TASK_MODULE_UNDERSTANDING_BASICS - Create Mongo connection and get the express app to listen on config.port
+mongoose
+  .connect(config.mongoose.url, config.mongoose.options)
+  .then(() => {
+    console.log("Connected to Mongodb");
+    server = app.listen(config.PORT, () => {
+      console.log("listing to port", config.PORT);
+    });
+  })
+  .catch();
