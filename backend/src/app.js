5619ca51371ec94b7e9840a65f81e48642788b36
Priyanka Chourey <priyanka.choure10@gmail.com>
2023-08-23 15:13:03 +0530

    configuration changes

--- backend/src/app.js
+++ backend/src/app.js
@@ -12,7 +12,7 @@ const app = express();
 // enable cors
 app.use(
   cors({
-    origin: ["https://threeway-studio-frontend.vercel.app"],
+    origin: ["https://threeway-studio-frontend.vercel.app/backend"],
     methods: ["GET", "POST"],
     credentials: true,
   })
